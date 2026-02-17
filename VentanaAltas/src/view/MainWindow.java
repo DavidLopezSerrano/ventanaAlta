55befbb02984e54ba561f62524b5c69b396dcc46
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-17 12:17:49 +0100

    Atributos rama ventanas - mainWindow

--- VentanaAltas/src/view/MainWindow.java
+++ VentanaAltas/src/view/MainWindow.java
@@ -1,11 +1,19 @@
 package view;
 
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame{
 
+	private ArrayList<JLabel>labels = new ArrayList<>();
 	
+	private ArrayList<JComboBox>combos = new ArrayList<>();
 	
+	private ArrayList<JTextField>textFields = new ArrayList<>();
 	
 	public MainWindow() {
 
