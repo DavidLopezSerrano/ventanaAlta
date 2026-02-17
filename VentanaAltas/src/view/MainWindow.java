379b28f094d68c1ca779611c1b88668093d019e0
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-17 13:21:34 +0100

    CreacionPaneles Main Window

--- VentanaAltas/src/view/MainWindow.java
+++ VentanaAltas/src/view/MainWindow.java
@@ -1,11 +1,14 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import model.Data;
@@ -24,9 +27,26 @@ public class MainWindow extends JFrame {
 
 		super();
 		setProperties();
+		createMainPanels(data);
 
 		pack();
 
+	}
+
+	private void createMainPanels(Data data2) {
+		JPanel mainPanel = new JPanel(new GridLayout(2,2));
+		JPanel p1 = new JPanel();
+		JPanel p2 = new JPanel();
+		p1.setBackground(Color.black);
+		p2.setBackground(Color.RED);
+		mainPanel.add(p1);
+		mainPanel.add(p2);
+		
+		this.add(mainPanel);
+		
+		
+		
+		
 	}
 
 	private void setProperties() {
