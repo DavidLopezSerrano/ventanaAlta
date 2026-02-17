86a62011ed3334c308c6dcb8f3e8a03e242be95f
evamariabeltranrsanjosemlg-hub <evamariabeltranr.sanjosemlg@fundacionloyola.net>
2026-02-17 12:24:19 +0100

    Test push Eva Branch

--- VentanaAltas/src/view/MainWindow.java
+++ VentanaAltas/src/view/MainWindow.java
@@ -1,7 +1,13 @@
 package view;
+import java.util.*;
 
+import javax.swing.*;
 public class MainWindow {
-	private ArrayList<JTextField>
+	private ArrayList<JLabel> labels = new ArrayList<>();
+	private ArrayList<JComboBox> combos = new ArrayList<>();
+	private ArrayList<JTextField> textboxs = new ArrayList<>();
+
+	
 	public MainWindow() {
 
 		
