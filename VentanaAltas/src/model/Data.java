efe6cef003781bcceba0542a3f31608d9139bc27
Carlosmr-10 <carlosmichelenar.sanjosemlg@fundacionloyola.net>
2026-02-17 12:34:10 +0100

    rama carlos

--- VentanaAltas/src/model/Data.java
+++ VentanaAltas/src/model/Data.java
@@ -1,8 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Data {
 
+	private ArrayList<String> texts = new ArrayList<>();
+	
 	public Data() {
 		
+	}
+	
+	private void addTexts() {
+		
+		texts.add(""); // 
 	}
 }
