04a42b7384d813ef48e3d1fc38ddc6f9bb51431b
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-19 10:30:20 +0100

    Commit textos

--- VentanaAltas/src/model/Data.java
+++ VentanaAltas/src/model/Data.java
@@ -11,7 +11,8 @@ public class Data {
 
 	// Constructor de data
 	public Data() {
-
+		super();
+		
 		createTexts();
 		createNumbers();
 
--- VentanaAltas/src/view/MainWindow.java
+++ VentanaAltas/src/view/MainWindow.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
@@ -20,27 +21,64 @@ public class MainWindow extends JFrame {
 	private ArrayList<JComboBox> combos = new ArrayList<>();
 
 	private ArrayList<JTextField> textFields = new ArrayList<>();
+	
+	private ArrayList<JPanel> panelsMain = new ArrayList<>();
+	
+	private ArrayList<JPanel> panelsBoxes = new ArrayList<>();
 
 	private Data data;
 
 	public MainWindow(Data data) {
 
 		super();
+		this.data=data;
 		setProperties();
-		createMainPanels(data);
+		createMainPanels();
+		createPanelPersonal();
+		createPanelAddress();
 
 		pack();
 
 	}
 
-	private void createMainPanels(Data data2) {
-		JPanel mainPanel = new JPanel(new GridLayout(2,2));
-		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
-		p1.setBackground(Color.black);
-		p2.setBackground(Color.RED);
-		mainPanel.add(p1);
-		mainPanel.add(p2);
+	private void createPanelAddress() {
+		for (int i = 0; i < data.getAddress().size(); i++) {
+			
+			panelsBoxes.add(new JPanel(new GridLayout(2,1, 10,10)));
+			
+			labels.add(new JLabel(data.getAddress().get(i)));
+			
+			panelsBoxes.get(i).add(labels.get(i));
+			
+			panelsMain.get(1).add(panelsBoxes.get(i));
+		}		
+	}
+
+	private void createPanelPersonal() {
+		
+		for (int i = 0; i < data.getPersonalData().size(); i++) {
+			
+			panelsBoxes.add(new JPanel(new GridLayout(2,1, 10,10)));
+			
+			labels.add(new JLabel(data.getPersonalData().get(i)));
+			
+			panelsBoxes.get(i).add(labels.get(i));
+			
+			panelsMain.get(0).add(panelsBoxes.get(i));
+		}
+		
+	}
+
+	private void createMainPanels() {
+		JPanel mainPanel = new JPanel(new GridLayout(2,1));
+		panelsMain.add(new JPanel());
+		panelsMain.add(new JPanel());
+		panelsMain.get(0).setBackground(Color.black);
+		panelsMain.get(1).setBackground(Color.RED);
+		
+		for (int i = 0; i < panelsMain.size(); i++) {
+		mainPanel.add(panelsMain.get(i));
+		}
 		
 		this.add(mainPanel);
 		
@@ -56,4 +94,62 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	}
+
+	public ArrayList<JLabel> getLabels() {
+		return labels;
+	}
+
+	public void setLabels(ArrayList<JLabel> labels) {
+		this.labels = labels;
+	}
+
+	public ArrayList<JComboBox> getCombos() {
+		return combos;
+	}
+
+	public void setCombos(ArrayList<JComboBox> combos) {
+		this.combos = combos;
+	}
+
+	public ArrayList<JTextField> getTextFields() {
+		return textFields;
+	}
+
+	public void setTextFields(ArrayList<JTextField> textFields) {
+		this.textFields = textFields;
+	}
+
+	public ArrayList<JPanel> getPanels() {
+		return panelsMain;
+	}
+
+	public void setPanels(ArrayList<JPanel> panels) {
+		this.panelsMain = panels;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public ArrayList<JPanel> getPanelsMain() {
+		return panelsMain;
+	}
+
+	public void setPanelsMain(ArrayList<JPanel> panelsMain) {
+		this.panelsMain = panelsMain;
+	}
+
+	public ArrayList<JPanel> getPanelsBoxes() {
+		return panelsBoxes;
+	}
+
+	public void setPanelsBoxes(ArrayList<JPanel> panelsBoxes) {
+		this.panelsBoxes = panelsBoxes;
+	}
+	
+	
 }
